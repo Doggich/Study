a9e99f649e4f631f71d25f153ab4a7f484e21b43
Doggich <dogglyg01@gmail.com>
2025-11-19 16:52:02 +0300

    Sum of two numbers

--- main.cpp
+++ main.cpp
@@ -3,14 +3,17 @@
 using namespace std;
 
 
+int sum(int a, int b) 
+{
+   return a + b;
+}
+
+
 int main() 
 {
    int number1, number2;
    printf("Input a, b? ");
    scanf("%d%d", &number1, &number2);
-   printf("\nUser input it`s numbers as: %d, %d.\n", number1, number2);
+   printf("\n%d + %d == %d.\n", number1, number2, sum(number1, number2));
    return 0;
 }
-
-
-
