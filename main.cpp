186b209b1071d85c2559eba37ca5b8a9a08dd121
Doggich <dogglyg01@gmail.com>
2025-11-19 17:03:07 +0300

    Simple calc

--- main.cpp
+++ main.cpp
@@ -3,9 +3,24 @@
 using namespace std;
 
 
-int sum(int a, int b) 
+int add(int a, int b) 
 {
    return a + b;
+}
+
+int sub(int a, int b) 
+{
+   return a - b;
+}
+
+int mul(int a, int b) 
+{
+   return a * b;
+}
+
+int div(int a, int b) 
+{
+   return a / b;
 }
 
 
@@ -14,6 +29,9 @@ int main()
    int number1, number2;
    printf("Input a, b? ");
    scanf("%d%d", &number1, &number2);
-   printf("\n%d + %d == %d.\n", number1, number2, sum(number1, number2));
+   printf("\n%d + %d == %d.\n", number1, number2, add(number1, number2));
+   printf("%d - %d == %d.\n", number1, number2, sub(number1, number2));
+   printf("%d * %d == %d.\n", number1, number2, mul(number1, number2));
+   printf("%d / %d == %d.\n", number1, number2, div(number1, number2));
    return 0;
 }
