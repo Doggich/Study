a4349a3cc8e7ed0efee9b46e2dd765b114fb559f
Doggich <dogglyg01@gmail.com>
2025-11-19 16:45:50 +0300

    Commit with .cpp and .exe

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+using namespace std;
+
+
+int main() 
+{
+   int number1, number2;
+   printf("Input a, b? ");
+   scanf("%d%d", &number1, &number2);
+   printf("\nUser input it`s numbers as: %d, %d.\n", number1, number2);
+   return 0;
+}
+
+
+
